a6509a9f7a4f2ac9c36ec19d14b61caa0c294e5d
gilberto-freitas <gilberto@crewfare.com>
2026-03-01 19:43:57 -0300

    feat: added drawer

---
+++ src/components/drawer/types.ts
@@ -0,0 +1,24 @@
+import type { ComponentProps } from 'react';
+import type { Drawer as DrawerPrimitive } from 'vaul';
+
+export type DrawerRootProps = ComponentProps<typeof DrawerPrimitive.Root>;
+
+export type DrawerTriggerProps = ComponentProps<typeof DrawerPrimitive.Trigger>;
+
+export type DrawerPortalProps = ComponentProps<typeof DrawerPrimitive.Portal>;
+
+export type DrawerCloseProps = ComponentProps<typeof DrawerPrimitive.Close>;
+
+export type DrawerOverlayProps = ComponentProps<typeof DrawerPrimitive.Overlay>;
+
+export type DrawerContentProps = ComponentProps<typeof DrawerPrimitive.Content>;
+
+export type DrawerHeaderProps = ComponentProps<'div'>;
+
+export type DrawerFooterProps = ComponentProps<'div'>;
+
+export type DrawerTitleProps = ComponentProps<typeof DrawerPrimitive.Title>;
+
+export type DrawerDescriptionProps = ComponentProps<
+	typeof DrawerPrimitive.Description
+>;
